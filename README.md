799706fdb71fcd42c381885b6e53e57e835ecf20
Roman Kholopov <roma.kholopov@gmail.com>
2022-08-26 18:04:27 +0300

    refactor: react element replaced with react components

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Todo Application
 -----
-На ветке **create_react_element** добавлен новый элемент **el**
+- 1. На ветке **create_react_element** добавлен новый элемент **el**
+- 2. На ветке **create_react_components** импортированы react & reaactDOM. Cозданы новые компоненты:
+    - 1. **AppHeader**
+    - 2. **SearchPanel**
+    - 3. **TodoList**
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const el = (
-    <div>
-        <h1>My Todo List</h1>
-        <input placeholder="search" />
+const TodoList = () => {
+    return (
         <ul>
             <li>Learn React</li>
             <li>Build Awesome App</li>
         </ul>
-    </div>
-);
-// console.log(el);
+    );
+}
 
-root.render(el);
+const AppHeader = () => {
+    return <h1>My Todo List</h1>;
+}
+
+const SearchPanel = () => {
+    return <input placeholder="search" />;
+}
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    )
+}
+
+root.render(
+    <StrictMode>
+        <App/>
+    </StrictMode>
+);
