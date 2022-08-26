05829d1fb1824dbe554158536c5ed5e73096849e
Roman Kholopov <roma.kholopov@gmail.com>
2022-08-26 20:27:03 +0300

    removed unnecessary code in index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const App = () => {
 
-    const isLoggedIn = true;
-    const loginBox = <span>Log in please</span>;
-    const welcomeBox = <span>Welcome Back</span>;
-
     return (
         <div>
-            { isLoggedIn ? welcomeBox : loginBox}
             <AppHeader />
             <SearchPanel />
             <TodoList />
